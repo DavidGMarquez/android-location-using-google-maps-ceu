ed51e67437d97ea44a8fdef243016623f448a29e
Kik10 <Kik10@45d7231a-b532-6304-41f8-aa8a53b02170>
2010-10-23 23:31:41 +0000

    Añadida funcionalidad de almacenamiento y muestra de usuarios. Hay que depurar pequeñas cosas pero funciona.

--- trunk/IS2_Proyecto/gen/uspceu/eps/is2/aplicacion/R.java
+++ trunk/IS2_Proyecto/gen/uspceu/eps/is2/aplicacion/R.java
@@ -55,10 +55,11 @@ public final class R {
         public static final int app_menu=0x7f050000;
     }
     public static final class string {
-        public static final int _IndentificarUsuario=0x7f040003;
+        public static final int _IndentificarUsuario=0x7f040004;
         public static final int app_name=0x7f040001;
-        public static final int btn_showMessage=0x7f040004;
+        public static final int btn_showMessage=0x7f040005;
         public static final int hello=0x7f040000;
         public static final int noavisos=0x7f040002;
+        public static final int nousuarios=0x7f040003;
     }
 }
