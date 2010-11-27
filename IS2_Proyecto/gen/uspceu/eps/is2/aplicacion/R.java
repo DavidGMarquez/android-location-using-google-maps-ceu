f1179db1ada4da880673986bf44b966f93a789a4
sir.dg.mv <DavidGMarquez@users.noreply.github.com>
2010-11-27 15:41:47 +0000

    Ahora sale un muñeco en mexico y si le das te dice que estas en méxico en el mapa, es la base para que puedan ponerse los avisos en el mapa.

--- IS2_Proyecto/gen/uspceu/eps/is2/aplicacion/R.java
+++ IS2_Proyecto/gen/uspceu/eps/is2/aplicacion/R.java
@@ -11,12 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int car2=0x7f020000;
-        public static final int ic_crear=0x7f020001;
-        public static final int ic_ver=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int interior2=0x7f020004;
-        public static final int usupng=0x7f020005;
+        public static final int androidmarker=0x7f020000;
+        public static final int car2=0x7f020001;
+        public static final int ic_crear=0x7f020002;
+        public static final int ic_ver=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int interior2=0x7f020005;
+        public static final int usupng=0x7f020006;
     }
     public static final class id {
         public static final int Identificacion=0x7f060016;
