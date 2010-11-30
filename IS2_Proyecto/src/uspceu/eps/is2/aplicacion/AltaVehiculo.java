58b990444667c98c78d345a12a9fd103b60d64a5
sir.dg.mv <DavidGMarquez@users.noreply.github.com>
2010-11-30 22:57:02 +0000

    Cambiado como meter los vehículos y puesto el validador de matriculas

--- IS2_Proyecto/src/uspceu/eps/is2/aplicacion/AltaVehiculo.java
+++ IS2_Proyecto/src/uspceu/eps/is2/aplicacion/AltaVehiculo.java
@@ -1,11 +1,15 @@
 package uspceu.eps.is2.aplicacion;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -23,6 +27,7 @@ import android.widget.Toast;
   
      //private EditText txtMessage;
      private EditText id_vehiculo;
+     private EditText id_matricula;
    //--  private EditText id_marca;
    //--  private EditText id_modelo;
    //--  private EditText id_color;
@@ -37,6 +42,7 @@ import android.widget.Toast;
          setContentView(R.layout.form_altavehiculo);
      
          id_vehiculo = (EditText) findViewById(R.id.id_vehiculo);
+         id_matricula = (EditText) findViewById(R.id.id_matricula);
          lab= (TextView) findViewById(R.id.label);
             //  lab.setText("Italic, highlighted, bold.", TextView.BufferType.SPANNABLE); 
          //---  id_marca = (EditText) findViewById(R.id.id_marca);
@@ -45,22 +51,34 @@ import android.widget.Toast;
        //  img= (ImageView)findViewById(R.drawable.meta);
       btnShowMessage = (Button) findViewById(R.id.btn_showMessage);
        
-        
+      File file = getBaseContext().getFileStreamPath("Vehiculo");
+      if(file.exists())
+      {
+    	  Toast.makeText(getBaseContext(),"Ya existe un vehículo si le da a guardar se actualizaran los datos", Toast.LENGTH_LONG).show();
+      }
 
 
          btnShowMessage.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
+            	   Pattern p = Pattern.compile("(\\d{4}-[\\D\\w]{3}|[\\D\\w]{1,2}-\\d{4}-[\\D\\w]{2})");
+            	      Matcher m = p.matcher(id_matricula.getText());
+            	      if (!m.find()){
+            	    	  Toast.makeText(getBaseContext(),"Matricula incorrecta, introduzca formato válido", Toast.LENGTH_LONG).show();            	      
+            	    }
+             else{
              /*    String str =  id_vehiculo.getText().toString()+" "+id_modelo.getText().toString()
                  +" "+ id_marca.getText().toString()+ ""+id_color.getText().toString()
                  +
                      "!";*/
                  //Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
-                 Vehiculo vehi=new Vehiculo(id_vehiculo.getText().toString(),1);
+            	 Vehiculo vehi=new Vehiculo(id_vehiculo.getText().toString(), id_matricula.getText().toString());
+                 
                 		//-- ,id_marca.getText().toString()
                 		//-- ,id_modelo.getText().toString(),id_color.getText().toString());
                 // Toast.makeText(getBaseContext(),vehi.getMarca(), Toast.LENGTH_LONG).show();  		 
                  guardarVehiculo(vehi);    
                  leerVehiculo();
+             }
              }
          });      
      
@@ -68,12 +86,13 @@ import android.widget.Toast;
 // }
  
  
- public void guardarVehiculo(Vehiculo a){
+ public void guardarVehiculo(Vehiculo vehiculo){
 	 
 		FileOutputStream fos;
 		try {
-			fos = openFileOutput("Crear_Vehiculo2", Context.MODE_APPEND);
-			fos.write((a.toCompleto2()).getBytes());
+			deleteFile("Vehiculo");
+			fos = openFileOutput("Vehiculo", Context.MODE_APPEND);
+			fos.write((vehiculo.toStringDescMatri()).getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -82,24 +101,23 @@ import android.widget.Toast;
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
- }//	Toast.makeText(CrearAvisos.this,"Aviso Guardado",Toast.LENGTH_LONG).show();
+ }
 
- public void leerVehiculo(){
-	 Vehiculo a;
+ public String leerVehiculo(){
+	 String stringVehiculo=new String();
 	 int numlins = -1;
 		String aux = "";
 		ArrayList<String> stringarray = new ArrayList<String>();
-
 		try {
 			InputStreamReader isr = new InputStreamReader(
-					openFileInput("Crear_Vehiculo2"));
+					openFileInput("Vehiculo"));
 			BufferedReader br = new BufferedReader(isr);
 			do {
 				numlins++;
 				aux = br.readLine();
-				if (aux != null)
+				if (aux != null){
 					stringarray.add(aux + '\n');
-				Toast.makeText(getBaseContext(),aux, Toast.LENGTH_LONG).show();              
+				stringVehiculo=stringVehiculo.concat(aux);}              
 			} while (aux != null);
 			br.close();
 
@@ -110,18 +128,9 @@ import android.widget.Toast;
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		int i = 0, ai = 0;
-		aux = "";
-		for (i = 0; i < numlins; i++) {
-			aux += stringarray.get(i);
-			if ((i + 1) % 8 == 0) {
-				ai++;
-				 a = new Vehiculo(aux);
-			 Toast.makeText(getBaseContext(),a.getMarca(), Toast.LENGTH_LONG).show();              
-				//al.add(a);
-				//aux = "";
-			}
-		}
+		Vehiculo vehiculo=new Vehiculo(stringVehiculo);
+			 Toast.makeText(getBaseContext(),"Actualizado!!!\n"+vehiculo.toString(), Toast.LENGTH_LONG).show();              	
+		return stringVehiculo;
 	}
  
  @Override
