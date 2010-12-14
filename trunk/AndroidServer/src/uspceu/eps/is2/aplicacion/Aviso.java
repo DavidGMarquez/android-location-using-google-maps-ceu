5e40274b8b4eb08e51b111ab7a5209bdec2c0e0c
sir.dg.mv <sir.dg.mv@45d7231a-b532-6304-41f8-aa8a53b02170>
2010-12-14 21:40:14 +0000

    Ultimo de los commit. Subo el servidor

---
+++ trunk/AndroidServer/src/uspceu/eps/is2/aplicacion/Aviso.java
@@ -0,0 +1,187 @@
+/**
+ * 
+ */
+package uspceu.eps.is2.aplicacion;
+
+//import java.text.SimpleDateFormat;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Cayette
+ *
+ */
+public class Aviso implements Serializable {
+
+
+	private String nombreAviso;
+	private String tipoAviso;
+	private String descripcionAviso;
+	private int calificacion;
+	private int votaciones;
+	private Usuario usuario;
+	private Date fechacreacion;
+	private PuntoMapa puntoMapa;
+	
+	
+	public String getNombreAviso() {
+		return nombreAviso;
+	}
+	public void setNombreAviso(String nombreAviso) {
+		this.nombreAviso = nombreAviso;
+	}
+	public String getTipoAviso() {
+		return tipoAviso;
+	}
+	public void setTipoAviso(String tipoAviso) {
+		this.tipoAviso = tipoAviso;
+	}
+	public String getDescripcionAviso() {
+		return descripcionAviso;
+	}
+	public void setDescripcionAviso(String descripcionAviso) {
+		this.descripcionAviso = descripcionAviso;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Date getFechacreacion() {
+		return fechacreacion;
+	}
+	public void setFechacreacion(Date fechacreacion) {
+		this.fechacreacion = fechacreacion;
+	}
+	public PuntoMapa getPuntoMapa() {
+		return puntoMapa;
+	}
+	public void setPuntoMapa(PuntoMapa puntoMapa) {
+		this.puntoMapa = puntoMapa;
+	}
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+	public int getCalificacion() {
+		return calificacion;
+	}
+	public void setVotaciones(int votaciones) {
+		this.votaciones = votaciones;
+	}
+	public int getVotaciones() {
+		return votaciones;
+	}
+	
+	//Constructor completo
+	public Aviso(String nombreAviso, String tipoAviso,
+			String descripcionAviso, Usuario usuario, Date fechacreacion,
+			PuntoMapa puntoMapa, int calificacion, int votaciones) throws NombreAvisoException {
+		if(nombreAviso.length()<1) throw new NombreAvisoException("Aviso sin nombre");
+		this.nombreAviso = nombreAviso;
+		this.tipoAviso = tipoAviso;
+		this.descripcionAviso = descripcionAviso;
+		this.usuario = usuario;
+		this.fechacreacion = fechacreacion;
+		this.puntoMapa = puntoMapa;
+		this.calificacion=calificacion;
+		this.votaciones=votaciones;
+	}
+	
+	//Constructor que pone los valores de fecha, calificación y votaciones por defecto
+	public Aviso(String nombreAviso, String descripcionAviso, String tipo, Usuario usuario, PuntoMapa puntoMapa) throws NombreAvisoException{
+		if(nombreAviso.length()<1) throw new NombreAvisoException("Aviso sin nombre");
+		this.nombreAviso = nombreAviso;
+		this.tipoAviso = tipo;
+		this.descripcionAviso = descripcionAviso;
+		this.usuario = usuario;
+		this.puntoMapa = puntoMapa;
+		this.fechacreacion=new Date();
+		this.calificacion=0;
+		this.votaciones=0;
+	}	
+	
+	// Constructor que pone los valores de tipo, fecha, calificación y votaciones por defecto
+	public Aviso(String nombreAviso, String descripcionAviso, Usuario usuario, PuntoMapa puntoMapa) throws NombreAvisoException {
+		if(nombreAviso.length()<1) throw new NombreAvisoException("Aviso sin nombre");
+		this.nombreAviso = nombreAviso;
+		this.tipoAviso = "Evento";
+		this.descripcionAviso = descripcionAviso;
+		this.usuario = usuario;
+		this.puntoMapa = puntoMapa;
+		this.fechacreacion=new Date();
+		this.calificacion=0;
+		this.votaciones=0;
+	}
+		
+	public String toCompleto() {		
+		return "Aviso: "+ nombreAviso + "\n" +
+				"Tipo: " + tipoAviso + "\n" +
+				descripcionAviso + "\n" +
+				"Punto: " + puntoMapa + "\n" +
+				"Creado el: " + fechacreacion + "\n" +
+				"Creado por: " + usuario + "\n" +
+				"Puntuacion: " + calificacion + " de 5" + "\n";
+	}
+
+	public String toMostrar() {		
+		
+		
+		return  descripcionAviso + "\n" +
+				"En: "+this.puntoMapa.toString() + '\n'+
+				"Creado el: " + 
+				 new SimpleDateFormat("HH:mm dd/MM/yy").format(this.getFechacreacion())+'\n' +
+				"Usuario: "+ usuario.getNombre();
+	}
+	
+	@Override
+	public String toString(){
+		//SimpleDateFormat sdf = new SimpleDateFormat("HH:mm dd/MM/yy");
+		//return this.getNombreAviso()+", "+sdf.format(this.getFechacreacion())+'\n';
+		return this.getNombreAviso()+"  "+new SimpleDateFormat("HH:mm dd/MM/yy").format(this.getFechacreacion())+'\n';
+	}
+	
+	/* Metodos para leer avisos de fichero */
+	
+	public Aviso(String aviso) throws FormatoCoordenadasException{
+		int ini=0, fin=0;
+		String au="";
+		
+		ini=aviso.indexOf(" ",0)+1;
+		fin=aviso.indexOf("\n",ini);
+		this.nombreAviso=aviso.substring(ini,fin);
+		
+		ini=aviso.indexOf(" ",fin)+1;
+		fin=aviso.indexOf("\n",ini);
+		this.tipoAviso=aviso.substring(ini,fin);
+		
+		ini=fin+1;
+		fin=aviso.indexOf("\n",ini);
+		this.descripcionAviso=aviso.substring(ini,fin);
+		
+		
+		ini=aviso.indexOf(" ",fin)+1;
+		fin=aviso.indexOf(" ",ini);
+		au=aviso.substring(ini,fin);
+		ini=aviso.indexOf(", ",fin)+2;
+		fin=aviso.indexOf(" ",ini);
+		this.puntoMapa=new PuntoMapa(au,aviso.substring(ini,fin));
+		
+		ini=aviso.indexOf(": ",fin)+2;
+		fin=aviso.indexOf("\n",ini);
+		this.fechacreacion=new Date(aviso.substring(ini,fin));
+		
+		ini=aviso.indexOf(": ",fin)+1;
+		fin=aviso.indexOf("\n",ini);
+		this.usuario=new Usuario(aviso.substring(ini, fin));
+		
+		ini=aviso.indexOf(" ",fin)+1;
+		fin=aviso.indexOf(" ",ini);
+		this.calificacion=new Integer(aviso.substring(ini, fin)).intValue();
+		
+		
+	}
+	
+	
+}
---
+++ trunk/AndroidServer/src/uspceu/eps/is2/aplicacion/Usuario.java
@@ -0,0 +1,76 @@
+package uspceu.eps.is2.aplicacion;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+	private String nombre;
+	private String password;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Usuario(String nombre, String pass) {
+		this.nombre = nombre;
+		this.password = pass;
+	}
+	
+	public Usuario(String name){
+		this.nombre = name;
+	}
+
+    @Override
+	public String toString() {
+		return nombre;
+	}
+	
+	
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public String toCompleto() {		
+		return "Usuario: "+ nombre + "\n" +
+				"pass: " + password + "\n" ;
+				
+	}
+	
+	public boolean esUsuIncorrecto(){
+		
+		//si usuario y contraseña vacios
+		if (this.nombre==null){
+			return true;
+			}
+		if (this.nombre.equals("")||(this.nombre.contains(" "))){
+			return true;
+		}
+		if (this.password==null){
+			return true;
+		}
+		if(this.password.equals("")||(this.password.contains(" "))){
+			return true;
+		}
+		//si usu y pass no tienen entre 6 y 20 caracteres
+		if((this.nombre.length()<6)||(this.password.length()<6)){
+			return true;
+		}
+		if((this.nombre.length()>20)||(this.password.length()>20)){
+			return true;
+		}
+				
+		return false;
+	}
+	
+}
